bdf7a47f61d75e71ec129974f84cd57dfc08b813
“coreavparis2020m” <coreavparis2020m@gmail.com>
2020-06-09 13:00:56 +0200

    initial

---
+++ app.js
@@ -0,0 +1,21 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/contacto', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/contacto.html');
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/public/pagina404.html');
+})
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en http://localhost:3000');
+})
